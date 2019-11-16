38f8c33be651728cad8952ea3c65940048f3d1ea
Antonio Pons <toninopons@gmail.com>
2019-11-16 18:27:43 +0100

    implementing functionality for extracting coordinates of businesses via yelp api

--- src/yelp_api.py
+++ src/yelp_api.py
@@ -9,10 +9,21 @@ load_dotenv()
 url = "https://api.yelp.com/v3/businesses/search"
 
 
-
 api_key = os.getenv("YELP_API_KEY")
 headers = {'Authorization': 'Bearer %s' % api_key}
 params = {'term':'Starbucks','location':'New York City'}
 req=requests.get(url, params=params, headers=headers)
 print('The status code is {}'.format(req.status_code))
-print(json.loads(req.text))
+
+api_response = json.loads(req.text)
+
+
+coords = []
+
+
+
+for i in api_response["businesses"]:
+    coords.append(i["coordinates"])
+
+
+print(coords)
