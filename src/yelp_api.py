6127ed2c89213ceab0e8ff9bfff2140aafcc3975
Antonio Pons <toninopons@gmail.com>
2019-11-16 19:07:30 +0100

    Trying to get the api right

--- src/yelp_api.py
+++ src/yelp_api.py
@@ -4,26 +4,28 @@ import os
 import json
 
 load_dotenv()
-
-
-url = "https://api.yelp.com/v3/businesses/search"
-
-
+search_url = "https://api.yelp.com/v3/businesses/search"
+match_url = "https://api.yelp.com/v3/businesses/matches"
 api_key = os.getenv("YELP_API_KEY")
 headers = {'Authorization': 'Bearer %s' % api_key}
-params = {'term':'Starbucks','location':'New York City'}
-req=requests.get(url, params=params, headers=headers)
-print('The status code is {}'.format(req.status_code))
 
-api_response = json.loads(req.text)
+def get_coords(company, city):
+    params = {'term' : company,'location' : city}
+    req=requests.get(search_url, params=params, headers=headers)
+    print('The status code is {}'.format(req.status_code))
+
+    api_response = json.loads(req.text)
+
+    coords = []
+
+    for i in api_response["businesses"]:
+        coords.append(i["coordinates"])
+
+    print(api_response)
+
+    return coords
 
 
-coords = []
 
 
-
-for i in api_response["businesses"]:
-    coords.append(i["coordinates"])
-
-
-print(coords)
+print(get_coords("Rockstar Games", "New York City"))
